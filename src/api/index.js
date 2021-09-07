efd62853a6ce162963161f64f773db4ca66e0d09
Sabrynna <limasabrynna03@gmail.com>
2021-09-07 00:43:41 -0300

    Registrando rota de lojas

--- src/api/index.js
+++ src/api/index.js
@@ -3,11 +3,13 @@ const express = require('express')
 const produtosRouter = require('./produtos')
 const usuarioRouter = require('./usuario')
 const listaRouter = require('./lista')
+const lojasRouter = require('./lojas')
 
 const router = express.Router()
 
 router.use('/produtos', produtosRouter)
 router.use('/usuarios', usuarioRouter)
 router.use('/listas', listaRouter)
+router.use('/lojas', lojasRouter)
 
 module.exports = router

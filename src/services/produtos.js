443cc42aa509a600bdf6dbfd284daf3a118482cb
LaomaNogueira <88911231+LaomaNogueira@users.noreply.github.com>
2021-09-09 12:28:51 -0400

    Implementadas novas rotas referentes a CG-6 e CG-7(jira)

--- src/services/produtos.js
+++ src/services/produtos.js
@@ -10,8 +10,28 @@ class ProdutoService {
     }
 
     async procuraProdutoId(produtoId) { //procura no BD o produto com o ID recebido na requisição
-      const produto = await this.produto.findOne({ where: { id: produtoId } });
+      const produto = await this.produto.findByPk(produtoId); //método findByPk busca o produto pelo ID (Pk = Primary Key)
       return produto;
+    }
+
+    async diminuiEstoque(produtoId, quantidade) {
+      const produto = await this.produto.findByPk(produtoId); //método findByPk busca o produto pelo ID (Pk = Primary Key)
+      
+      if (produto.estoque === 0) {
+        throw new Error('Produto sem estoque disponível')
+      }
+      
+      if ((produto.estoque - quantidade) < 0) { //VER!!!!!!!
+        throw new Error(`Produto possui apenas ${produto.estoque} unidades em estoque.`)
+      }
+      
+      produto.estoque = produto.estoque - quantidade;
+      
+      const salvo = await produto.save(); //método save percebe as alterações em produto e salva no BD
+
+      if(!salvo) {
+        throw new Error('Erro ao tentar diminuir o estoque.')
+      }
     }
   
 }

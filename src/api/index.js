cdaa1092db10e6c3e61ce6046533bbdeb1e1851c
CeliaLopes <oliveira.lopes.celia@gmail.com>
2021-09-10 16:47:04 -0300

    adicionado rota de endereco

--- src/api/index.js
+++ src/api/index.js
@@ -5,6 +5,7 @@ const usuarioRouter = require('./usuario')
 const listaRouter = require('./lista')
 const lojasRouter = require('./lojas')
 const categoriasRouter = require('./categoria')
+const enderecoRouter = require('./endereco') 
 
 const router = express.Router()
 
@@ -13,5 +14,6 @@ router.use('/usuarios', usuarioRouter)
 router.use('/listas', listaRouter)
 router.use('/lojas', lojasRouter)
 router.use('/categorias', categoriasRouter)
+router.use('/endereco', enderecoRouter)
 
 module.exports = router

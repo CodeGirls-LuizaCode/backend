14e7904716edfb63672af1b2e50d42749485b64e
thaisminas <thaisminasds@gmail.com>
2021-09-11 14:38:02 -0300

    testando erro de cors

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,20 @@
+// const process = require('dotenv/config');
+// console.log(process.env.DATABASE_URL)
+
 module.exports = {
     dialect: 'postgres',
     host: 'ec2-54-147-126-173.compute-1.amazonaws.com',
     port: 5432,
-    database: process.env.DATABASE_URL,
-    username: process.env.USER_DB,
-    password: process.env.PASSWORD_DB,//não esquecer de alterar a senha
-    // dialectOptions: {
-    //     ssl: {
-    //       require: true, // This will help you. But you will see nwe error
-    //       rejectUnauthorized: false // This line will fix new error
-    //     }
-    // }
+    database: 'dev5e6hj3j13vu',
+    username: 'bifijodsoepfhq',
+    password: 'd3fa3b287a9d006c4e17289852f5c1e46352079c444ed510332a61e9a0e6dbe8',//não esquecer de alterar a senha
+    dialectOptions: {
+        ssl: {
+          require: true, // This will help you. But you will see nwe error
+          rejectUnauthorized: false // This line will fix new error
+        }
+    }
 }
+
 
 
